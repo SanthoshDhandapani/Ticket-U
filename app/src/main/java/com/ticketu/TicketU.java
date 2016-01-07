b471c71199199b88d58b9cc529675f418141590d
SanthoshDhandapani <santhoshdhandapani@marketsimplified.com>
2016-01-07 18:53:35 +0530

    added a basic card list with picasso to show images

--- app/src/main/java/com/ticketu/TicketU.java
+++ app/src/main/java/com/ticketu/TicketU.java
@@ -3,6 +3,8 @@ package com.ticketu;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
 
 /**
  * Created by hp-user on 12/28/2015.
@@ -18,6 +20,10 @@ public class TicketU extends Application {
     private void initialize() {
         Parse.enableLocalDatastore(this);
         Parse.initialize(this);
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        ParseACL.setDefaultACL(defaultACL, true);
     }
 
 }
--- app/src/main/java/com/ticketu/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/ticketu/ui/fragment/HomeFragment.java
@@ -5,14 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.ticketu.R;
 import com.ticketu.ui.activity.BaseActivity;
+import com.ticketu.ui.adapters.RecyclerViewCardsAdapter;
 import com.ticketu.ui.presenter.BasePresenter;
 import com.ticketu.ui.presenter.home.HomePresenterImpl;
+import com.ticketu.ui.viewmodel.CardViewBean;
 import com.ticketu.ui.viewmodel.HomeViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by santhoshd on 30-12-2015.
@@ -20,6 +27,7 @@ import com.ticketu.ui.viewmodel.HomeViewModel;
 public class HomeFragment extends BaseFragment implements HomePresenterImpl.View, HomeViewModel.Listener {
 
     HomePresenterImpl  homePresenter;
+    private static final String MOCK_URL = "http://lorempixel.com/800/400/nightlife/";
 
     @Override
     protected int getFragmentLayout() {
@@ -57,6 +65,24 @@ public class HomeFragment extends BaseFragment implements HomePresenterImpl.View
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public void setTicketsList() {
+        RecyclerView mRecyclerView = (RecyclerView)getView().findViewById(R.id.tickets_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(new RecyclerViewCardsAdapter(getActivity(),createMockList()));
+    }
+
+    private List<CardViewBean> createMockList() {
+        List<CardViewBean> listCard = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            listCard.add(new CardViewBean(MOCK_URL + i));
+        }
+        return listCard;
     }
 
     @Override
