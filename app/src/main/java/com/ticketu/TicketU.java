6a430ffcc6a1efbc492db10447c37c5e05c06da8
santhoshd <santhoshdhandapani@inxsasia.com>
2016-01-07 20:30:12 +0530

    just added

--- app/src/main/java/com/ticketu/TicketU.java
+++ app/src/main/java/com/ticketu/TicketU.java
@@ -2,6 +2,7 @@ package com.ticketu;
 
 import android.app.Application;
 
+import com.github.johnkil.print.PrintConfig;
 import com.parse.Parse;
 import com.parse.ParseACL;
 import com.parse.ParseUser;
@@ -24,6 +25,8 @@ public class TicketU extends Application {
         ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
         ParseACL.setDefaultACL(defaultACL, true);
+
+        PrintConfig.initDefault(getAssets(), "fonts/fontawesome-webfont.ttf");
     }
 
 }
