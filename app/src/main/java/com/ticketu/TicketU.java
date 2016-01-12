d83c908c044cda22e6d401e0ce7ca4c0614f06dd
santhoshd <santhoshdhandapani@inxsasia.com>
2016-01-12 11:53:40 +0530

    Removed any text view and integrated Calligraphy for font works

--- app/src/main/java/com/ticketu/TicketU.java
+++ app/src/main/java/com/ticketu/TicketU.java
@@ -9,6 +9,8 @@ import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.ticketu.model.Ticket;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by hp-user on 12/28/2015.
  */
@@ -30,5 +32,11 @@ public class TicketU extends Application {
         ParseACL.setDefaultACL(defaultACL, true);
 
         PrintConfig.initDefault(getAssets(), "fonts/material_icons.ttf");
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath(getString(R.string.regular_text_typeface))
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
     }
 }
